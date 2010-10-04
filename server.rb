cf02c6fbdc195abcd68c848869e84a7f9a7edecc
Toon Claes <toon@tonotdo.com>
2010-10-04 22:11:05 +0200

    This version sends the request, but seems to fail after login?

--- server.rb
+++ server.rb
@@ -1,17 +1,53 @@
 require 'rubygems'
 require 'oauth'
 require 'sinatra'
-#require 'net/http'
+require 'net/http'
+require 'hpricot'
+require 'pp'
 
 post '/' do
-  site = params['site']
-  consumer=OAuth::Consumer.new('wh5EMIRW7CCWYGZbAVMDA', 'vCXJ81ARTjWJm6i8qw9EzfzZp6gWPR9CNUUNuh3Gh8', {:site => site})
+  # Start
+  site            = params['foo_site']
+  consumer_key    = params['foo_consumer_key']
+  consumer_secret = params['foo_consumer_secret']
+
+  consumer=OAuth::Consumer.new(consumer_key, consumer_secret, {:site => site})
   request_token=consumer.get_request_token
   auth_url = request_token.authorize_url
-  puts auth_url
+  # Authenticate
   url = URI.parse(auth_url)
-  Net::HTTP.start(url.host, url.port) { |http|
-    res = http.get url.path
-    puts res
-  }
+  Net::HTTP.start(url.host, url.port) do |http|
+    # Get input form
+    res = http.get url.request_uri
+    # Check response
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      # OK
+    else
+      res.error!
+    end
+    # Parse the form
+    doc = Hpricot(res.body)
+    inputs = doc.search('input')
+    # Fill in form
+    form = Hash.new
+    inputs.each do |inp|
+      key = inp.attributes['name']
+      if (params.has_key?(key))
+        form[key] = params[key]
+      else
+        form[key] = inp.attributes['value']
+      end
+    end
+    # Post form
+    res = Net::HTTP.post_form url, form
+    # Check response
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      # OK
+    else
+      res.error!
+    end
+    res.body
+  end  # http session
 end
--- test.sh
+++ test.sh
@@ -1,1 +1,20 @@
-curl -d "site=http://api.twitter.com" 0.0.0.0:4567
+#!/bin/sh
+set -e
+
+# Hardcoded values (temporary)
+SITE='http://api.twitter.com'
+CONSUMER_KEY='wh5EMIRW7CCWYGZbAVMDA'
+CONSUMER_SECRET='vCXJ81ARTjWJm6i8qw9EzfzZp6gWPR9CNUUNuh3Gh8'
+# Read login from stdin
+USER=$1
+PASS=$2
+
+# TODO why is the dummy needed?
+data="dummy=\"1\"&"\
+"foo_site=\"${SITE}\"&"\
+"foo_consumer_key=\"${CONSUMER_KEY}\"&"\
+"foo_consumer_secret=\"${CONSUMER_SECRET}\"&"\
+"username_or_email=\"${USER}\"&"\
+"password=\"${PASS}\""
+
+curl -d \"${data}\" 0.0.0.0:4567
