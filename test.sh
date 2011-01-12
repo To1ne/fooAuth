a82bbcdebeaf45759569822252206cf9f8c2d7e5
Toon Claes <toon@tonotdo.com>
2011-01-12 23:29:27 +0100

    Test command to post a tweet

--- test.sh
+++ test.sh
@@ -3,17 +3,17 @@ set -e
 
 # Hardcoded values (temporary)
 SITE='http://api.twitter.com'
-#PAGE='/statuses/update.json'
-PAGE='/statuses/home_timeline.json'
 CONSUMER_KEY='j0XVctwQDETgM8Twy2Qew'
 CONSUMER_SECRET='VhNSTD3eDQDuJ2EYbVnyZTZfydQ5kVX2SKbTGIBSuc'
 # Read login from stdin
 USER=$1
 PASS=$2
-STATUS=$3
 
 # TODO for loop for all other arguments from stdin
 
-# GET
-curl -u ${USER}:${PASS} -G --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" http://127.0.0.1:4567/${SITE}${PAGE}
+# GET home timeline
+#curl -u ${USER}:${PASS} -G --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" http://127.0.0.1:4567/${SITE}/statuses/home_timeline.json
+
+# POST new status
+curl -u ${USER}:${PASS} --data-urlencode status="$3" --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" http://127.0.0.1:4567/${SITE}/statuses/update.json
 
