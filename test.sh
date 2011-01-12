339475de4b4c23f1569504c4716f004895d0853b
Toon Claes <toon@tonotdo.com>
2011-01-12 23:50:44 +0100

    Use heroku to test too

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 set -e
 
 # Hardcoded values (temporary)
+#HOST='http://127.0.0.1:4567'
+HOST='http://fooauth.heroku.com'
 SITE='http://api.twitter.com'
 CONSUMER_KEY='j0XVctwQDETgM8Twy2Qew'
 CONSUMER_SECRET='VhNSTD3eDQDuJ2EYbVnyZTZfydQ5kVX2SKbTGIBSuc'
@@ -12,8 +14,8 @@ PASS=$2
 # TODO for loop for all other arguments from stdin
 
 # GET home timeline
-#curl -u ${USER}:${PASS} -G --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" http://127.0.0.1:4567/${SITE}/statuses/home_timeline.json
+curl -u ${USER}:${PASS} -G --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" ${HOST}/${SITE}/statuses/home_timeline.json
 
 # POST new status
-curl -u ${USER}:${PASS} --data-urlencode status="$3" --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" http://127.0.0.1:4567/${SITE}/statuses/update.json
+#curl -u ${USER}:${PASS} --data-urlencode status="$3" --data-urlencode foo_consumer_key="${CONSUMER_KEY}" --data-urlencode foo_consumer_secret="${CONSUMER_SECRET}"  --data-urlencode count="1" ${HOST}/${SITE}/statuses/update.json
 
