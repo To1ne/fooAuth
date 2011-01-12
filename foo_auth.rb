8eeb1bcf204960209f7796aa4cbff257da12f6b3
Toon Claes <toon@tonotdo.com>
2011-01-12 22:04:17 +0100

    Using mechanize (soo much easier)

--- foo_auth.rb
+++ foo_auth.rb
@@ -3,7 +3,8 @@ require 'rubygems'
 require 'sinatra'
 require 'oauth'
 require 'net/http'
-require 'hpricot'
+require 'mechanize'
+#require 'hpricot'
 
 helpers do
   class URI::Generic
@@ -16,10 +17,10 @@ helpers do
   class FooAuth
     def initialize(params, request)
       # keep method for later use
-      @method = request.request_method.downcase.to_sym
+      method = request.request_method.downcase.to_sym
       # get API URL from path
       url = URI.parse(request.fullpath[1..-1]) # cut off leading '/'
-      @page = url.request_uri
+      # @page = url.request_uri
       @site = url.site
 
       # oAuth key and secret # TODO with or without foo_?
@@ -37,7 +38,7 @@ helpers do
       (@username, @password) = auth.credentials
 
       # callback url
-      @callback = "http://#{request.host_with_port}/auth"
+      @callback = "http://#{request.host_with_port}/auth/#{method}#{request.fullpath}" # TODO this only works with GET?
     end
     def get_response
       # get authentication url
@@ -46,49 +47,68 @@ helpers do
       auth_url = request_token.authorize_url
       # do authentication
       url = URI.parse(auth_url)
-      # TODO I don't think this session is needed
-      Net::HTTP.start(url.host, url.port) do |http|
-        form = {'lang' => 'en'}      # force English
-        # get input form
-        res = http.get url.request_uri, form
-        # check response
-        case res
-        when Net::HTTPSuccess, Net::HTTPRedirection
-          # ok
-        else
-          res.error!
+
+      # Start mechanize
+      agent = Mechanize.new
+      agent.follow_meta_refresh = true       # make sure we are redirected to callback url
+
+      # get the login page
+      page = agent.get(url, {'lang' => 'en'})      # force English
+      form = page.forms.first     # just assume first (and only) # TODO make it failsafe
+      # fill in username and password
+      form.fields.each do |key, val|
+        if key.name.match(/\buser(?:name)?/) # end word boundary \b does not work on twitter.com
+          form[key.name] = @username
+        elsif key.name.match(/\bpass(?:word)?\b/)
+          form[key.name] = @password
         end
-        # parse the form
-        doc = Hpricot(res.body)
-        inputs = doc.search('input')
-        # fill in form
-        inputs.each do |inp|
-          key = inp.attributes['name']
-          #keyS = key.gsub(/^[^\[]+\[([^\]]+)\]/, '\1') # parse 'field' from 'session[field]' (used on twitter.com)
-          # TODO find better way to let the user pass credentials
-          if key.match(/\buser(?:name)?/) # end word boundary \b does not work on twitter.com
-            form[key] = @username
-          elsif key.match(/\bpass(?:word)?\b/)
-            form[key] = @password
-          else # keep the hidden fiels (oAuth sizzle)
-            form[key] = inp.attributes['value']
-          end
-        end
-        form.delete('cancel')     # this one will DENY access to the user
-        # TODO can we just use basic auth?
-        # does not work url.userinfo = "#{@username}:#{@password}"
-        # post form
-        res = Net::HTTP.post_form url, form
-        # check response
-        case res
-        when Net::HTTPSuccess
-          # TODO parse html and do <meta http-equiv="refresh" content=... /> redirection
-        when Net::HTTPRedirection
-          # TODO not sure what to do here
-        else
-          res.error!
-        end
-      end  # http session
+      end
+      # send the form
+      pp form
+      page = agent.submit form, form.button_with(:name => nil)
+      pp page
+
+      # Net::HTTP.start(url.host, url.port) do |http|
+      #   # get input form
+      #   res = http.get url.request_uri, form
+      #   # check response
+      #   case res
+      #   when Net::HTTPSuccess, Net::HTTPRedirection
+      #     # ok
+      #   else
+      #     res.error!
+      #   end
+      #   # parse the form
+      #   doc = Hpricot(res.body)
+      #   inputs = doc.search('input')
+      #   # fill in form
+      #   inputs.each do |inp|
+      #     key = inp.attributes['name']
+      #     #keyS = key.gsub(/^[^\[]+\[([^\]]+)\]/, '\1') # parse 'field' from 'session[field]' (used on twitter.com)
+      #     # TODO find better way to let the user pass credentials
+      #     if key.match(/\buser(?:name)?/) # end word boundary \b does not work on twitter.com
+      #       form[key] = @username
+      #     elsif key.match(/\bpass(?:word)?\b/)
+      #       form[key] = @password
+      #     else # keep the hidden fiels (oAuth sizzle)
+      #       form[key] = inp.attributes['value']
+      #     end
+      #   end
+      #   form.delete('cancel')     # this one will DENY access to the user
+      #   # TODO can we just use basic auth?
+      #   # does not work url.userinfo = "#{@username}:#{@password}"
+      #   # post form
+      #   res = Net::HTTP.post_form url, form
+      #   # check response
+      #   case res
+      #   when Net::HTTPSuccess
+      #     # TODO parse html and do <meta http-equiv="refresh" content=... /> redirection
+      #   when Net::HTTPRedirection
+      #     # TODO not sure what to do here
+      #   else
+      #     res.error!
+      #   end
+      # end  # http session
     end # def
   end # class
 end # helpers
@@ -97,8 +117,12 @@ post '/' do
   "Hello" # TODO get content from README.org or .md...
 end
 
-get '/auth/*/' do
-  # TODO
+get %r{\/auth\/(\w+)\/(.+)} do |method, fullurl|
+  # TODO we are getting here, no do something with it
+  puts "method:"
+  pp method
+  puts "fullurl:"
+  pp fullurl
   puts "request:"
   pp request
   puts "params:"
@@ -110,12 +134,12 @@ get '/auth/*/' do
   #res = access_token.request(@method, @page, @params)
 end
 
-post '/*' do
+post '/*' do # TODO regex matching
   foo = FooAuth.new(params, request)
   foo.get_response
 end
 
-get  '/*' do
+get  '/*' do # TODO regex matching
   foo = FooAuth.new(params, request)
   foo.get_response
 end
